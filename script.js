a67664d85b115da475fde2b253151858924f867d
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-07-07 13:27:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,72 @@
 //your JS code here. If required.
+
+
+let txt=document.querySelector("#text");
+let delay=document.querySelector("#delay");
+let btn=document.querySelector("#btn");
+let op=document.querySelector("#output");
+
+
+
+function abc(a){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            op.innerHTML=txt.value;
+        }, a);
+    })
+}
+
+
+
+btn.addEventListener("click",async function(){
+
+    let a=Number(delay.value);
+
+     try{
+
+        await abc(a);
+        
+        
+    }
+    catch(err){
+        console.log(err);
+    }
+
+
+})
+
+
+
+let txt=document.querySelector("#text");
+let delay=document.querySelector("#delay");
+let btn=document.querySelector("#btn");
+let op=document.querySelector("#output");
+
+
+
+function abc(a){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            op.innerHTML=txt.value;
+        }, a);
+    })
+}
+
+
+
+btn.addEventListener("click",async function(){
+
+    let a=Number(delay.value);
+
+     try{
+
+        await abc(a);
+        
+        
+    }
+    catch(err){
+        console.log(err);
+    }
+
+
+})
