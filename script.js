93a6184d5d9fcc49b385b870caf1ca1c718fb278
Mangesh Bodke <mangeshbodke2025@gmail.com>
2025-07-07 13:32:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//your JS code here. If required.
-
-
 let txt=document.querySelector("#text");
 let delay=document.querySelector("#delay");
 let btn=document.querySelector("#btn");
@@ -12,42 +9,8 @@ function abc(a){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             op.innerHTML=txt.value;
-        }, a);
-    })
-}
-
-
-
-btn.addEventListener("click",async function(){
-
-    let a=Number(delay.value);
-
-     try{
-
-        await abc(a);
-        
-        
-    }
-    catch(err){
-        console.log(err);
-    }
-
-
-})
-
-
-
-let txt=document.querySelector("#text");
-let delay=document.querySelector("#delay");
-let btn=document.querySelector("#btn");
-let op=document.querySelector("#output");
-
-
-
-function abc(a){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            op.innerHTML=txt.value;
+            resolve("resolved");
+     
         }, a);
     })
 }
